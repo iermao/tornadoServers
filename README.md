d2642671f3dd11b90b7c76f5eebbc51e5c1c101c
unknown <iermao@163.com>
2020-05-08 14:32:57 +0800

    edit md

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # tornadoServers
 tornadoweb服务以及websocket
+
+提供web api服务
+提供web socket 服务
--- Server/Webhandler.py
+++ Server/Webhandler.py
@@ -17,7 +17,7 @@ class IndexHandler(RequestHandler):
     def get_current_user(self):
         '''
         重写RequestHandler类中的get_current_user方法，用来判断当前是否是登录状态，请求中所有被@tornado.web.authenticated 装饰的方法，都需要此方法返回值不为None，否则给与403拒绝
-        :return: 用户名或者None . 为None判断为非法请求，POST 时Tornado进行403禁止访问 ；GET 时 302 重定向到/login
+        :return: 用户名或者None . 为None判断为非法请求，POST 时Tornado进行403 禁止访问 ；GET 时 302 重定向到/login  123213213
         '''
         user = self.get_argument(name='username', default='None')
         if user and user != 'None':
